670f53d0643b12deda29db1bd730d983e2e438ad
Martin <7252614+Lhoerion@users.noreply.github.com>
2022-01-10 23:01:20 +0100

    Update DocFx.Plugins.ExtractArticleAffix

--- common.ps1
+++ common.ps1
@@ -144,7 +144,7 @@ $versions=@{
     "docfx-plugins-typescriptreference.zip"="v1.1.5";
     "docfx-plugins-extractsearchindex.zip"="v1.0.4";
     "docfx-plugins-addimagemodal.zip"="v0.0.4";
-    "docfx-plugins-extractarticleaffix.zip"="v0.0.3";
+    "docfx-plugins-extractarticleaffix.zip"="v0.0.4";
     "docfx-tmpls-discordfx.zip"="v0.6.19";
 }
 
